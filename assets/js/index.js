fa95b2d3b934570b2e395d6ac088e76a6894b696
unknown <dawitkebedewoldu@gmail.com>
2024-04-17 02:30:55 -0700

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,33 @@
+const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+function handleMediaChange(event) {
+  const isSmallScreen = event.matches; 
+  const right_header = document.getElementById("right_header")
+  if (isSmallScreen) { 
+    right_header.innerHTML = '<button id="btn_right_header" onclick=handleHeaderBtn()> = </button>'
+  } 
+  else{
+    right_header.innerHTML = "<div id='right_header'> <div><Button>A</Button></div><div><button>B</button></div><div><button>C</button></div><div><button>D</button></div></div>"
+  }
+}
+function handleHeaderBtn(){
+    const btn = document.getElementById('btn_right_header')
+    const left_bar = document.getElementById('left_bar')
+    const logo = document.getElementById("logo")
+    if(btn.innerHTML == '='){
+
+        left_bar.style.length = '0px'
+        left_bar.style.visibility = "visible"
+        logo.style.visibility = 'hidden'
+        btn.innerHTML = 'X'
+        
+    }
+    else{
+        left_bar.style.length = '0px'
+        left_bar.style.visibility = "hidden"
+        logo.style.visibility = 'visible'
+        btn.innerHTML = '=' 
+    }
+}
+// Add event listener to the media query object
+mediaQuery.addEventListener("change", handleMediaChange);
